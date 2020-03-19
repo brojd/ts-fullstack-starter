92fc0931d2260db49731086af1fc347dad87f2d6
Dominik Broj <Dominik.Broj@dataart.com>
2020-03-19 12:47:19 +0100

    change logging

--- server/src/utils/logger.ts
+++ server/src/utils/logger.ts
@@ -4,7 +4,7 @@ import { isEnvProd, isEnvDev } from '@shared/utils/env';
 const options: winston.LoggerOptions = {
   transports: [
     new winston.transports.Console({
-      level: isEnvProd() ? 'error' : 'info',
+      level: isEnvProd() ? 'debug' : 'info',
       format: winston.format.simple()
     }),
     new winston.transports.File({ filename: 'debug.log', level: 'debug' })
